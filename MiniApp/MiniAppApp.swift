5d49372efd632ea9dc0393c8f9f8212ec5ce8968
Rafael Magaña <raismaav.2510@gmail.com>
2024-11-08 15:50:11 -0600

    Initial Commit

---
+++ MiniApp/MiniAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MiniAppApp.swift
+//  MiniApp
+//
+//  Created by Rafael Magana  on 08/11/24.
+//
+
+import SwiftUI
+
+@main
+struct MiniAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
