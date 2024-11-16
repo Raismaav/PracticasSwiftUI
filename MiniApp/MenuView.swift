a1214e0cda4dd621259b8029e4b083ad6d2140db
Rafael Magaña <raismaav.2510@gmail.com>
2024-11-15 23:19:48 -0600

    IMC App and lists

---
+++ MiniApp/MenuView.swift
@@ -0,0 +1,18 @@
+//
+//  MenuView.swift
+//  MiniApp
+//
+//  Created by Rafael Magana  on 14/11/24.
+//
+
+import SwiftUI
+
+struct MenuView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MenuView()
+}
